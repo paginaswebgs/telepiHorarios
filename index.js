dd215a4bb2113c6492b3411c677bd0090355bc76
paginaswebgs <paginaswebsgs@gmail.com>
2025-07-23 00:57:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -236,5 +236,5 @@ app.post('/usuarios/:usuarioId/horarios/:mes/:dia', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, '0.0.0.0', () => console.log(`API corriendo en puerto ${PORT}`));
